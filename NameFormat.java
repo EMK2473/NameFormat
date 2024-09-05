5d9ac85a1db67457dcf429bca69d295d11364729
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-04 23:12:50 -0400

    looped through data, if not last element in array prints char at index 0

--- NameFormat.java
+++ NameFormat.java
@@ -10,6 +10,12 @@ public class NameFormat {
     String[] data = input.split(" ");
     System.out.print(data[data.length -1] + ", ");
 
+    for(int i = 0; i < data.length; i++) {
+        if(i != data.length -1){
+            System.out.print(data[i].charAt(0) + ".");;
+        }
+    }
+
     scnr.close();
     }
 }
