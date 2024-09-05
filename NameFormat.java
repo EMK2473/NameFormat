af9402da9f72d7f3c7858ef5730897b61c55e928
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-04 23:09:23 -0400

    assigned input to scanner. printed last element in data array

--- NameFormat.java
+++ NameFormat.java
@@ -6,5 +6,10 @@ public class NameFormat {
    public static void main(String[] args) {
     Scanner scnr = new Scanner(System.in);
 
-   }
+    String input = scnr.nextLine();
+    String[] data = input.split(" ");
+    System.out.print(data[data.length -1] + ", ");
+
+    scnr.close();
+    }
 }
